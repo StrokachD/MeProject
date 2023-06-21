a58cc4e138776546597262743208c899975835ef
StrokachD <128859515+StrokachD@users.noreply.github.com>
2023-06-21 20:25:03 +0300

    Add files via upload
    задача 3

---
+++ Conspect.java
@@ -0,0 +1,18 @@
+public class Conspect {
+    /*3)  Создать класс Конспект. Класс должен содержать следующие поля:
+        	Название предмета;
+        	ФИО студента;
+        	Количество страниц;
+        	Год выпуска;
+        	Цвет обложки.
+        	Название заведения, где учится студент;         */
+
+    private String courseTitle;
+    private String studentName;
+    private int numberOfPages;
+    private int yearOfIssue;
+    private String coverColor;
+    private String nameOfTheInstitution;
+
+
+}
